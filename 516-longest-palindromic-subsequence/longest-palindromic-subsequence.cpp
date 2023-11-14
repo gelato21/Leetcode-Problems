abdf80d12dcc5d56ec0c592088ce02eb4983257d
Pankaj Kumar Pandit <143678006+gelato21@users.noreply.github.com>
2023-11-14 12:38:23 +0530

    Time: 178 ms (15.54%) | Memory: 73.3 MB (43.89%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
@@ -30,12 +30,31 @@ public:
         return dp[i][j]=ans;
 
     }
+    int solveTable(string& t1, string& t2){
+        int n=t1.length();
+        vector<vector<int>> dp(n+1, vector<int>(n+1, 0));
+        for(int i=t1.length()-1;i>=0;i--){
+            for(int j=t2.length()-1;j>=0;j--){
+                int ans=0;
+                if(t1[i]==t2[j]){
+                    ans=1+dp[i+1][j+1];
+                }
+                else{
+                    ans=max(dp[i+1][j], dp[i][j+1]);
+                }
+                dp[i][j]=ans;
+            }
+        }
+        return dp[0][0];
+    }
     int longestPalindromeSubseq(string s) {
         string s2=s;
         reverse(s2.begin(), s2.end());
         // return OnlyRecursion(s, s2, 0, 0);
 
-        vector<vector<int>> dp(s.length(), vector<int>(s.length(), -1));
-        return SolveMemo(s, s2, 0, 0, dp);
+        // vector<vector<int>> dp(s.length(), vector<int>(s.length(), -1));
+        // return SolveMemo(s, s2, 0, 0, dp);
+
+        return solveTable(s, s2);
     }
 };
