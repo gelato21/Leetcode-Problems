4a17fa12a6a2e38e4be1dd0c17564ea0ea778808
Pankaj Kumar Pandit <143678006+gelato21@users.noreply.github.com>
2023-11-17 09:41:38 +0530

    Time: 0 ms (100.00%) | Memory: 6.9 MB (19.34%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    //Recursion
+    //Recursion-> Top-down 
     int f(int i, int j){
         if(i==0 && j==0) return 1;
         if(i<0 || j<0) return 0;
@@ -31,9 +31,31 @@ public:
 
 
     }
+    // tabulation method-> Bottom Up
+    int tabulation(int m, int n){
+        vector<vector<int>> dp(m, vector<int>(n));
+        dp[0][0]=1;
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0) continue;
+                int up= i>0 ? dp[i-1][j] : 0;
+                int left= j>0 ? dp[i][j-1] : 0;
+
+                dp[i][j]=up+ left;
+            }
+        }
+        return dp[m-1][n-1];
+
+    }
+
+
     int uniquePaths(int m, int n) {
         vector<vector<int>> dp(m, vector<int>(n, -1));
-    //    return f(m-1, n-1);
-    return f(m-1, n-1, dp);
+        //    return f(m-1, n-1);
+    
+        // return f(m-1, n-1, dp);
+
+        return tabulation(m, n);
     }
 };
