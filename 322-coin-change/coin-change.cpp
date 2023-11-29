26056081791f9b516dab11c140e0f33b957b740d
Pankaj Kumar Pandit <143678006+gelato21@users.noreply.github.com>
2023-11-29 07:30:29 +0530

    Time: 105 ms (24.73%) | Memory: 45.5 MB (31.47%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -20,8 +20,32 @@ public:
     }
     int coinChange(vector<int>& coins, int amount) {
         int n=coins.size();
-        vector<vector<int>> dp(n, vector<int>(amount+1, -1));
-        int ans=f(coins.size()-1, amount, coins, dp);
+        // vector<vector<int>> dp(n, vector<int>(amount+1, -1));
+        // int ans=f(coins.size()-1, amount, coins, dp);
+        int ans=0;
+        vector<vector<int>> dp(n, vector<int>(amount+1, 0));
+
+        for(int target=0;target<=amount;target++){
+            if(target%coins[0]==0){
+                dp[0][target]=target/coins[0];
+            }
+            else dp[0][target]=1e9;
+        }
+
+        for(int i=1;i<n;i++){
+            for(int target=1;target<=amount;target++){
+                int notTake=0 + dp[i-1][target];
+                int take=INT_MAX;
+                if(coins[i]<=target)
+                take= 1 + dp[i][target-coins[i]];
+
+                dp[i][target]=min(take, notTake);
+            }
+        }
+
+        ans=dp[n-1][amount];
+
+
         if(ans>=1e9) return -1;
         else return ans;
     }
