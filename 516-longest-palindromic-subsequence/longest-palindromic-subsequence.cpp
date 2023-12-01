db87eeb13545b29e303e11dd62a3a0f1c6c0559a
Pankaj Kumar Pandit <143678006+gelato21@users.noreply.github.com>
2023-12-01 13:12:46 +0530

    Time: 168 ms (31.34%) | Memory: 73.5 MB (11.59%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
@@ -1,60 +1,23 @@
 class Solution {
 public:
-    int OnlyRecursion(string& text1, string& text2, int i, int j){
-        if(i>=text1.length() || j>=text2.length()){
-            return 0;
-        }
-        int ans=0;
-        if(text1[i]==text2[j]){
-            ans=1+OnlyRecursion(text1, text2, i+1, j+1);
-        }
-        else{
-            ans=max(OnlyRecursion(text1, text2, i+1, j), OnlyRecursion(text1, text2, i, j+1));
-        }
-        return ans;
-    }
-    int SolveMemo(string& t1, string& t2, int i, int j, vector<vector<int>>& dp){
-        if(i>=t1.length() || j>=t2.length()){
-            return 0;
-        }
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-        int ans=0;
-        if(t1[i]==t2[j]){
-            ans=1+SolveMemo(t1, t2, i+1, j+1, dp);
-        }
-        else{
-            ans=max(SolveMemo(t1, t2, i+1, j, dp), SolveMemo(t1, t2, i, j+1, dp));
-        }
-        return dp[i][j]=ans;
+    int lcs(string s1, string s2){
+        int n=s1.length();
+        int m=s2.length();
 
-    }
-    int solveTable(string& t1, string& t2){
-        int n=t1.length();
-        vector<vector<int>> dp(n+1, vector<int>(n+1, 0));
-        for(int i=t1.length()-1;i>=0;i--){
-            for(int j=t2.length()-1;j>=0;j--){
-                int ans=0;
-                if(t1[i]==t2[j]){
-                    ans=1+dp[i+1][j+1];
+        vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s1[i-1]==s2[j-1]){
+                    dp[i][j]=1+dp[i-1][j-1];
                 }
-                else{
-                    ans=max(dp[i+1][j], dp[i][j+1]);
-                }
-                dp[i][j]=ans;
+                else dp[i][j]=max(dp[i-1][j], dp[i][j-1]);
             }
         }
-        return dp[0][0];
+        return dp[n][m];
     }
     int longestPalindromeSubseq(string s) {
-        string s2=s;
-        reverse(s2.begin(), s2.end());
-        // return OnlyRecursion(s, s2, 0, 0);
-
-        // vector<vector<int>> dp(s.length(), vector<int>(s.length(), -1));
-        // return SolveMemo(s, s2, 0, 0, dp);
-
-        return solveTable(s, s2);
+        string t=s;
+        reverse(t.begin(), t.end());
+        return lcs(t, s);
     }
 };
