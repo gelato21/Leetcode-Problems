e45546b7aad3c4e09cf0ec3089f24b42db76ca49
Pankaj Kumar Pandit <143678006+gelato21@users.noreply.github.com>
2023-12-13 17:49:00 +0530

    Time: 13 ms (46.05%) | Memory: 11.2 MB (35.68%) - LeetSync

---
+++ 47-permutations-ii/permutations-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void helper(int pos, vector<int>& nums, vector<vector<int>>& ans){
+
+        if(pos>=nums.size()){
+            ans.push_back(nums);
+            return ;
+        }
+
+        unordered_set<int> m;
+        for(int i=pos; i<nums.size();i++){
+            if(m.find(nums[i])!=m.end()) continue;
+            m.insert(nums[i]);
+            swap(nums[i], nums[pos]);
+            helper(pos+1, nums, ans);
+            swap(nums[i], nums[pos]);
+        }
+    }
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        vector<vector<int>> ans;
+        helper(0, nums, ans);
+        return ans;
+    }
+};
