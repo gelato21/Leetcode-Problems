8d8c965c7a659b99c881c0e35f08f6d4151e2237
Pankaj Kumar Pandit <143678006+gelato21@users.noreply.github.com>
2023-12-14 08:20:35 +0530

    Time: 186 ms (80.16%) | Memory: 117.4 MB (76.35%) - LeetSync

--- 2606-difference-between-ones-and-zeros-in-row-and-column/difference-between-ones-and-zeros-in-row-and-column.cpp
+++ 2606-difference-between-ones-and-zeros-in-row-and-column/difference-between-ones-and-zeros-in-row-and-column.cpp
@@ -3,37 +3,20 @@ public:
     vector<vector<int>> onesMinusZeros(vector<vector<int>>& grid) {
         int n=grid.size();
         int m=grid[0].size();
-        vector<pair<int, int>> row(n), col(m);
-        for(int i=0;i<n;i++){
-            int zeros=0;
-            int ones=0;
-            for(int j=0;j<m;j++){
-                if(grid[i][j]==0){
-                    zeros++;
-                }
-                else ones++;
-            }
-            row[i]= {zeros, ones};
-        }
-        for(int j=0;j<m;j++){
-            int zeros=0;
-            int ones=0;
-            for(int i=0;i<n;i++){
-                if(grid[i][j]==0){
-                    zeros++;
-                }
-                else ones++;
-            }
-            col[j]={zeros, ones};
-        }
-        
-        vector<vector<int>> ans(n, vector<int>(m));
+        vector<int> onceRow(n, 0), onceCol(m, 0);
+
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                ans[i][j]=(row[i].second+col[j].second-row[i].first-col[j].first);
+                onceRow[i]+=grid[i][j];
+                onceCol[j]+=grid[i][j];
             }
         }
-        return ans;
+        vector<vector<int>> diff(n, vector<int>(m, 0));
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                diff[i][j]=(onceRow[i]+onceCol[j]-(n-onceRow[i])-(m-onceCol[j]));
+            }
+        }
+        return diff;
     }
-        
 };
