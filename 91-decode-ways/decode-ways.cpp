c203121124b9d71b949e67bcd5086953973246b8
Pankaj Kumar Pandit <143678006+gelato21@users.noreply.github.com>
2023-12-20 11:27:42 +0530

    Time: 3 ms (47.34%) | Memory: 6.7 MB (33.79%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -26,7 +26,30 @@ public:
         return dp[i]=two+one;
     }
     int numDecodings(string s) {
-        vector<int> dp(s.length(), -1);
-        return f(0, s, dp);
+        // vector<int> dp(s.length(), -1);
+        // return f(0, s, dp);
+        vector<int> dp(s.length()+1, 0);
+        dp[s.length()]=1;
+        for(int i=s.length()-1;i>=0;i--){
+            if(s[i]=='0'){
+                dp[i]=0;
+                continue;
+            }
+            int currVal=s[i]-48;
+            int one=dp[i+1];
+            int two=0;
+            if(i+1<s.length()){
+                int nextVal=s[i+1]-48;
+                if(currVal==1){
+                    two=dp[i+2];
+                }
+                else if(currVal==2 && nextVal<=6){
+                    two=dp[i+2];
+                }
+            }
+            dp[i]=two+one;
+        }
+        return dp[0];
+
     }
 };
