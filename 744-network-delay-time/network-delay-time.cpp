cc9eb63eb38a1867443ecfe3f476f7a331d29f0b
Pankaj Kumar Pandit <143678006+gelato21@users.noreply.github.com>
2023-12-24 12:30:19 +0530

    Time: 104 ms (38.77%) | Memory: 42.6 MB (49.76%) - LeetSync

---
+++ 744-network-delay-time/network-delay-time.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+        unordered_map<int, vector<pair<int, int>>> graph;
+        for(int i=1;i<=n;i++){
+            graph[i]=vector<pair<int, int>>();
+        }
+
+        for(auto &edge: times){
+            graph[edge[0]].push_back({edge[1], edge[2]});
+        }
+
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> minHeap;
+        minHeap.push({0, k});
+
+        unordered_map<int, int> minPath;
+
+        while(minHeap.empty()==false){
+            auto [d, node]=minHeap.top();
+            minHeap.pop();
+
+            if(minPath.find(node)!=minPath.end()) continue;
+            minPath[node]=d;
+
+            for(auto &child: graph[node]){
+                int weight=child.second, node2=child.first;
+                minHeap.push({d+weight, node2});
+            }
+        }
+
+
+        int ans=0;
+        for(int i=1;i<=n;i++){
+            if(minPath.find(i)==minPath.end()){
+                return -1;
+            }
+            else ans=max(ans, minPath[i]);
+        }
+        return ans;
+
+        
+    }
+};
