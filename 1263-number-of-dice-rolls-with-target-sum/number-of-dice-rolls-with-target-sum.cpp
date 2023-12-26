a383d6f9ac08449f86e4dacf9617bbbc8c1aaa7b
Pankaj Kumar Pandit <143678006+gelato21@users.noreply.github.com>
2023-12-26 09:41:33 +0530

    Time: 32 ms (35.37%) | Memory: 8.7 MB (65.29%) - LeetSync

--- 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.cpp
+++ 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.cpp
@@ -19,7 +19,18 @@ public:
         return dp[n][target]=ans%MOD;
     }
     int numRollsToTarget(int n, int k, int target) {
-        vector<vector<int>> dp(n+1, vector<int>(target+1, -1));
-        return f(n, target, k, dp);
+        vector<vector<int>> dp(n+1, vector<int>(target+1, 0));
+        dp[0][0]=1;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=target;j++){
+                long long ans=0;
+                for(int l=1;l<=k;l++){
+                    if(l<=j)
+                    ans+=dp[i-1][j-l]%MOD;
+                }
+                dp[i][j]=ans%MOD;
+            }
+        }
+        return dp[n][target];
     }
 };
