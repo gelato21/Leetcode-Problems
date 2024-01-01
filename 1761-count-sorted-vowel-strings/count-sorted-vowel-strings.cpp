3ad54a01dc71d34ec24dd7633e677a75b7e2ebdb
Pankaj Kumar Pandit <143678006+gelato21@users.noreply.github.com>
2024-01-01 11:30:52 +0530

    Time: 2 ms (57.31%) | Memory: 6.5 MB (43.00%) - LeetSync

--- 1761-count-sorted-vowel-strings/count-sorted-vowel-strings.cpp
+++ 1761-count-sorted-vowel-strings/count-sorted-vowel-strings.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
-    int f(int ind, int n){
+    int f(int ind, int n, vector<vector<int>>& dp){
         if(n==0) return 1;
         if(ind>=5) return 0;
 
+        if(dp[ind][n]!=-1) return dp[ind][n];
         //pick
-        int pick=f(ind, n-1);
+        int pick=f(ind, n-1, dp);
         //notpick
-        int notpick=f(ind+1, n);
+        int notpick=f(ind+1, n, dp);
 
-        return pick+notpick;
+        return dp[ind][n]=pick+notpick;
     }
     int countVowelStrings(int n) {
-        return f(0, n);
+        vector<vector<int>> dp(5, vector<int>(n+1, -1));
+        return f(0, n, dp);
     }
 };
