1d27213abe135ce771125cd0fb83b23849f48c24
Pankaj Kumar Pandit <143678006+gelato21@users.noreply.github.com>
2024-01-08 09:11:14 +0530

    Time: 107 ms (28.55%) | Memory: 64.9 MB (88.84%) - LeetSync

--- 975-range-sum-of-bst/range-sum-of-bst.cpp
+++ 975-range-sum-of-bst/range-sum-of-bst.cpp
@@ -11,22 +11,32 @@
  */
 class Solution {
 public:
-    void calculate(TreeNode* root, int low, int high, int& ans){
-        if(root==nullptr) return;
+    // void calculate(TreeNode* root, int low, int high, int& ans){
+    //     if(root==nullptr) return;
+
+    //     if(low<=root->val && root->val<=high){
+    //         ans+=root->val;
+    //         calculate(root->left, low, high, ans);
+    //         calculate(root->right, low, high, ans);
+    //     }
+    //     else if(root->val<low){
+    //         calculate(root->right, low, high, ans);
+    //     }
+    //     else calculate(root->left, low, high, ans);
+    // }
+    int rangeSumBST(TreeNode* root, int low, int high) {
+        if(root==nullptr){
+            return 0;
+        }
 
         if(low<=root->val && root->val<=high){
-            ans+=root->val;
-            calculate(root->left, low, high, ans);
-            calculate(root->right, low, high, ans);
+            return root->val+rangeSumBST(root->left, low, high)+rangeSumBST(root->right, low, high);
         }
         else if(root->val<low){
-            calculate(root->right, low, high, ans);
+            return rangeSumBST(root->right, low, high);
         }
-        else calculate(root->left, low, high, ans);
-    }
-    int rangeSumBST(TreeNode* root, int low, int high) {
-        int ans=0;
-        calculate(root, low, high, ans);
-        return ans;
+        else{
+            return rangeSumBST(root->left, low, high);
+        }
     }
 };
