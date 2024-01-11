1b6e75d7b43587df07a4472097ad61f8e7554456
Pankaj Kumar Pandit <143678006+gelato21@users.noreply.github.com>
2024-01-11 11:10:21 +0530

    Time: 3 ms (85.41%) | Memory: 10.1 MB (79.85%) - LeetSync

--- 1092-maximum-difference-between-node-and-ancestor/maximum-difference-between-node-and-ancestor.cpp
+++ 1092-maximum-difference-between-node-and-ancestor/maximum-difference-between-node-and-ancestor.cpp
@@ -11,29 +11,43 @@
  */
 class Solution {
 public:
-    int maxdiff;
-    void findMax(TreeNode* root, TreeNode* child){
-        if(child==nullptr) return;
+    // int maxdiff;
+    // void findMaxDiff(TreeNode* root, TreeNode* child){
+    //     if(child==nullptr) return;
 
-        maxdiff=max(maxdiff, abs(root->val-child->val));
+    //     maxdiff=max(maxdiff, abs(root->val-child->val));
 
-        findMax(root, child->left);
-        findMax(root, child->right);
+    //     findMaxDiff(root, child->left);
+    //     findMaxDiff(root, child->right);
         
-    }
-    void solve(TreeNode* root){
-        if(root==nullptr) return;
+    // }
+    // void solve(TreeNode* root){
+    //     if(root==nullptr) return;
 
-        //fix the root and explore the children
-        findMax(root, root->left);
-        findMax(root, root->right);
+    //     //fix the root and explore the children
+    //     findMaxDiff(root, root->left);
+    //     findMaxDiff(root, root->right);
 
-        solve(root->left);
-        solve(root->right);
+    //     solve(root->left);
+    //     solve(root->right);
+    // }
+
+    int solve(TreeNode* root, int minVal, int maxVal){
+        if(root==nullptr){
+            return abs(minVal-maxVal);
+        }
+
+        minVal=min(minVal, root->val);
+        maxVal=max(maxVal, root->val);
+
+        int l=solve(root->left, minVal, maxVal);
+        int r=solve(root->right, minVal, maxVal);
+        return max(l, r);
     }
     int maxAncestorDiff(TreeNode* root) {
-        maxdiff=-1;
-        solve(root);
-        return maxdiff;
+        if(root==nullptr) return -1;
+        
+        return solve(root, root->val, root->val);
+        
     }
 };
