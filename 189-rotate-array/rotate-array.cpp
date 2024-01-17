bd72e818f1abd3d608378b8559adc1e5a90c5eb6
Pankaj Kumar Pandit <143678006+gelato21@users.noreply.github.com>
2024-01-17 09:26:16 +0530

    Time: 22 ms (68.04%) | Memory: 25.4 MB (63.48%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -7,26 +7,39 @@ public:
     //     }
     //     ans[0]=temp;
     // }
+    void reverse(vector<int>& nums, int start, int end){
+        while(start<=end){
+            swap(nums[start++], nums[end--]);
+        }
+    }
     void rotate(vector<int>& nums, int k) {
         // for(int i=0;i<k;i++){
         //     rotatebyOne(nums);
         // }
+        //1 2 3 4 7 6 5
+        //4 3 2 1 7 6 5
+        //5 6 7 1 2 3 4
         int n=nums.size();
         k=k%n;
-        vector<int> temp1, temp2;
-        for(int i=0;i<n-k;i++){
-            temp1.push_back(nums[i]);
-        }
-        for(int i=n-k; i<nums.size();i++){
-            temp2.push_back(nums[i]);
-        }
-        int index=0;
-        for(int i=0;i<k;i++){
-            nums[index++]=temp2[i];
-        }
-        for(int i=0;i<temp1.size();i++){
-            nums[index++]=temp1[i];
-        }
+        reverse(nums, n-k, n-1);
+        reverse(nums, 0, n-k-1);
+        reverse(nums, 0, n-1);
+        // int n=nums.size();
+        // k=k%n;
+        // vector<int> temp1, temp2;
+        // for(int i=0;i<n-k;i++){
+        //     temp1.push_back(nums[i]);
+        // }
+        // for(int i=n-k; i<nums.size();i++){
+        //     temp2.push_back(nums[i]);
+        // }
+        // int index=0;
+        // for(int i=0;i<k;i++){
+        //     nums[index++]=temp2[i];
+        // }
+        // for(int i=0;i<temp1.size();i++){
+        //     nums[index++]=temp1[i];
+        // }
 
     }
 };
