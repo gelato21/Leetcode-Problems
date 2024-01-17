69583133c155a9d8527224eb0d4fc9356d150549
Pankaj Kumar Pandit <143678006+gelato21@users.noreply.github.com>
2024-01-17 10:11:30 +0530

    Time: 11 ms (52.10%) | Memory: 16 MB (29.64%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
@@ -1,27 +1,34 @@
 class Solution {
 public:
-    int binarySearch(vector<int>& numbers, int target, int start, int end){
-        while(start<=end){
-            int mid=(start+end)/2;
-            if(numbers[mid]==target){
-                return mid;
-            }
-            if(numbers[mid]<target){
-                start=mid+1;
-            }
-            else end=mid-1;
-        }
-        return -1;
-    }
-    vector<int> twoSum(vector<int>& numbers, int target) {
+    // int binarySearch(vector<int>& numbers, int target, int start, int end){
+    //     while(start<=end){
+    //         int mid=(start+end)/2;
+    //         if(numbers[mid]==target){
+    //             return mid;
+    //         }
+    //         if(numbers[mid]<target){
+    //             start=mid+1;
+    //         }
+    //         else end=mid-1;
+    //     }
+    //     return -1;
+    // }
+    vector<int> twoSum(vector<int>& nums, int target) {
         vector<int> ans;
-        int n=numbers.size();
-        for(int i=0;i<numbers.size();i++){
-            int ind=binarySearch(numbers, target-numbers[i], i+1, n-1);
-            if(ind!=-1){
-                ans.push_back(i+1);
-                ans.push_back(ind+1);
+        int n=nums.size();
+        int left=0, right=n-1;
+        while(left<right){
+            int sum=nums[left]+nums[right];
+            if(sum==target){
+                ans.push_back(left+1);
+                ans.push_back(right+1);
                 return ans;
+            }
+            if(sum>target){
+                right--;
+            }
+            else{
+                left++;
             }
         }
         return ans;
