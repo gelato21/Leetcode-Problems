4934bdba5d818d9fba706b8f5b9858f45fbd9774
Pankaj Kumar Pandit <143678006+gelato21@users.noreply.github.com>
2024-01-19 09:47:13 +0530

    Time: 16 ms (31.12%) | Memory: 11 MB (61.52%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -1,112 +1,43 @@
 class Solution {
 public:
-    //Recursion
-    int f(int i, int j, int n, vector<vector<int>>& matrix){
-        if(j<0 || j>=n) return 1e9;
-        if(i==n-1) return matrix[i][j];
+    // int f(int i, int j, int n, vector<vector<int>>& matrix, vector<vector<int>>& dp){
+
         
-        int below=f(i+1, j, n, matrix);
-        int left=f(i+1, j-1, n, matrix);
-        int right=f(i+1, j+1, n, matrix);
 
-        return matrix[i][j]+min(below, min(left, right));
-    }
+    //     if(j<0 || j>=n) return 1e9;
+    //     if(i==0) return matrix[i][j];
 
-    // Memoization
-    int f2(int i, int j, int n, vector<vector<int>>& matrix,vector<vector<int>>& dp){
-        if(j<0 || j>=n) return 1e9;
-        if(i==n-1) return matrix[i][j];
+    //     if(dp[i][j]!=-1) return dp[i][j];
 
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
+    //     int below=matrix[i][j]+f(i-1, j, n, matrix, dp);
+    //     int left=matrix[i][j]+f(i-1, j+1, n, matrix, dp);
+    //     int right=matrix[i][j]+f(i-1, j-1, n, matrix, dp);
 
-        int down=f2(i+1, j, n, matrix, dp);
-        int left=f2(i+1, j-1, n, matrix, dp);
-        int right=f2(i+1, j+1, n, matrix, dp);
-
-        return dp[i][j]=matrix[i][j]+min(down, min(left, right));
-    }
-    //Tabulation
-
-    vector<vector<int>> tabulation(vector<vector<int>>& matrix){
-        int n=matrix.size();
-        vector<vector<int>> dp(n, vector<int>(n));
-
-        dp[n-1]=matrix[n-1];
-
-        for(int i=n-2;i>=0;i--){
-            for(int j=0;j<n;j++){
-                int down=1e9, left=1e9, right=1e9;
-                down=dp[i+1][j];
-                if(j-1>=0){
-                    left=dp[i+1][j-1];
-                }
-                if(j+1<n){
-                    right=dp[i+1][j+1];
-                }
-
-                dp[i][j]=matrix[i][j]+min(down, min(right, left));
-            }
-        }
-        return dp;
-    }
-
-    //Space Optimization
-
-    int sapceOpti(vector<vector<int>>& matrix){
-        int n=matrix.size();
-
-        vector<int> prev;
-        prev=matrix[n-1];
-
-        for(int i=n-2;i>=0;i--){
-            vector<int > temp(n);
-            for(int j=0;j<n;j++){
-                int down=1e9, left=1e9, right=1e9;
-                down=prev[j];
-                if(j-1>=0){
-                    left=prev[j-1];
-                }
-                if(j+1<n){
-                    right=prev[j+1];
-                }
-
-                temp[j]=matrix[i][j]+min(down, min(right, left));
-            }
-            prev=temp;
-        }
-        int ans=INT_MAX;
-        for(int i=0;i<prev.size();i++){
-            ans=min(ans, prev[i]);
-        }
-        return ans;
-    }
-
+    //     return dp[i][j]=min(right, min(left, below));
+    // }
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int n=matrix.size();
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+        for(int j=0;j<n;j++){
+            dp[0][j]=matrix[0][j];
+        }
 
-        // int ans=1e9;
-        // for(int j=0;j<n;j++){
-        //     ans=min(ans, f(0, j, n, matrix));
-        // }
-        // return ans;    
+        for(int i=1;i<n;i++){
+            for(int j=0;j<n;j++){
 
-        // int ans=1e9;
-        // for(int j=0;j<n;j++){
-        //     vector<vector<int>> dp(n, vector<int>(n, -1));
-        //     ans=min(ans, f(0, j, n, matrix));
-        // }
-        // return ans;
+                int below=matrix[i][j] + dp[i-1][j];
+                int left=matrix[i][j]+ ((j+1>=n) ? 1e9 : dp[i-1][j+1]);
+                int right=matrix[i][j]+ ((j-1<0)  ? 1e9 : dp[i-1][j-1]);
 
+                dp[i][j]=min(right, min(left, below));
+            }
+        }
 
-        // int ans=INT_MAX;
-        // vector<vector<int>>dp=tabulation(matrix);
-        // for(int j=0;j<n;j++){
-        //     ans=min(ans, dp[0][j]);
-        // }
-        // return ans;
+        int ans=1e9;
+        for(int j=0;j<n;j++){
+            ans=min(ans, dp[n-1][j]);
+        }
+        return ans;
 
-        return sapceOpti(matrix);
     }
 };
