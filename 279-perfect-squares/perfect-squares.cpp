ea66c3aec42692a55a7d43eeb96fad9f1b692039
Pankaj Kumar Pandit <143678006+gelato21@users.noreply.github.com>
2024-02-08 09:46:23 +0530

    Time: 495 ms (7.57%) | Memory: 156.2 MB (5.32%) - LeetSync

--- 279-perfect-squares/perfect-squares.cpp
+++ 279-perfect-squares/perfect-squares.cpp
@@ -1,30 +1,31 @@
 class Solution {
 public:
 
-    int f(int ind, int target, vector<int>& perfectSquares, vector<vector<int>>& dp){
+    int f(int ind, int target, vector<int>& perfectSquares, vector<vector<int>>& dp, int end){
         if(target==0){
             return 0;
         }
-        if(ind>=101){
+        if(ind>=end+1){
             return 1e5;
         }
 
         if(dp[ind][target]!=-1) return dp[ind][target];
 
-        int notPick = f(ind+1, target, perfectSquares, dp);
+        int notPick = f(ind+1, target, perfectSquares, dp, end);
         int pick=1e5;
         if(perfectSquares[ind]<=target){
-           pick=1+f(ind, target-perfectSquares[ind], perfectSquares, dp);
+           pick=1+f(ind, target-perfectSquares[ind], perfectSquares, dp, end);
         }
 
         return dp[ind][target]=min(pick, notPick);
     }
     int numSquares(int n) {
-        vector<int> perfectSquares(101, 0);
-        for(int i=1;i<=100;i++){
+        int end=sqrt(n);
+        vector<int> perfectSquares(end+1, 0);
+        for(int i=1;i<=end;i++){
             perfectSquares[i]=i*i;
         }
-        vector<vector<int>> dp(101, vector<int>(n+1, -1));
-        return f(1, n, perfectSquares, dp);
+        vector<vector<int>> dp(end+1, vector<int>(n+1, -1));
+        return f(1, n, perfectSquares, dp, end);
     }
 };
