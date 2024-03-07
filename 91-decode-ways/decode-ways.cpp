42364b3cc50149a8ea1f5d25299b4a3c986827be
Pankaj Kumar Pandit <143678006+panditpankaj21@users.noreply.github.com>
2024-03-07 13:16:45 +0530

    Time: 4 ms (29.08%) | Memory: 7.6 MB (66.78%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -1,55 +1,36 @@
 class Solution {
 public:
-    // 1 2 3 ... 26
-    int f(int i, string s, vector<int>& dp){
-        if(i>=s.length()){
-            return 1;
-        }
-        if(s[i]=='0') return 0;
-
-        // first digit consider
-        if(dp[i]!=-1){
-            return dp[i];
-        }
-        int currVal=s[i]-48;
-        int one=f(i+1, s, dp);
-        int two=0;
-        if(i+1<s.length()){
-            int nextVal=s[i+1]-48;
-            if(currVal==1){
-                two=f(i+2, s, dp);
-            }
-            else if(currVal==2 && nextVal<=6){
-                two=f(i+2, s, dp);
-            }
-        }
-        return dp[i]=two+one;
-    }
     int numDecodings(string s) {
-        // vector<int> dp(s.length(), -1);
-        // return f(0, s, dp);
-        vector<int> dp(s.length()+1, 0);
-        dp[s.length()]=1;
-        for(int i=s.length()-1;i>=0;i--){
-            if(s[i]=='0'){
+        int n=s.length();
+        vector<int> dp(n, 0);
+        if(s[0]!='0')
+        dp[0]=1;
+        for(int i=1;i<n;i++){
+            int num = s[i]-'0';
+            int prev = s[i-1]-'0';
+            if(num == 0 && prev==0){
                 dp[i]=0;
-                continue;
-            }
-            int currVal=s[i]-48;
-            int one=dp[i+1];
-            int two=0;
-            if(i+1<s.length()){
-                int nextVal=s[i+1]-48;
-                if(currVal==1){
-                    two=dp[i+2];
-                }
-                else if(currVal==2 && nextVal<=6){
-                    two=dp[i+2];
+            }else if(num==0 && prev!=0){
+                if(prev*10+num<=26){
+                    if(i-2>=0){
+                        dp[i]=dp[i-2];
+                    }else dp[i]=1;
                 }
             }
-            dp[i]=two+one;
+            else if(num!=0 && prev==0){
+                dp[i]=dp[i-1];
+            }else{
+                if(prev*10+num <=26){
+                    if(i-2>=0)
+                    dp[i]=dp[i-1]+dp[i-2];
+                    else dp[i]=dp[i-1]+1;
+                }else{
+                    dp[i]=dp[i-1];
+                }
+                
+            }
         }
-        return dp[0];
+        return dp[n-1];
 
     }
 };
