43cd416f59f378f55d01ca987f7f42adb7900e10
Pankaj Kumar Pandit <143678006+panditpankaj21@users.noreply.github.com>
2024-03-07 11:12:24 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (86.66%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,48 +1,17 @@
 class Solution {
 public:
-    int f(int ind, int target, vector<int>& coins, vector<vector<int>>& dp){
-        if(ind==0){
-            if(target%coins[0]==0){
-                return 1;
-            }
-            else return 0;
-        }
+    int change(int amount, vector<int>& arr) {
+        int n=arr.size();
+        vector<int> dp(amount+1, 0);
+        dp[0]=1;
 
-        if(dp[ind][target]!=-1){
-            return dp[ind][target];
-        }
-
-        int notTake=f(ind-1, target, coins, dp);
-        int take=0;
-        if(coins[ind]<=target){
-            take=f(ind, target-coins[ind], coins, dp);
-        }
-
-        return dp[ind][target]=take+notTake;
-    }
-    int change(int amount, vector<int>& coins) {
-        int n=coins.size();
-        vector<vector<int>> dp(n, vector<int>(amount+1, 0));
-        vector<int> prev(amount+1, 0);
-        for(int target=0;target<=amount;target++){
-            if(target%coins[0]==0){
-                prev[target]=1;
-            }
-        }
-
-        for(int ind=1;ind<n;ind++){
-            vector<int> curr(amount+1, 0);
-            for(int target=0;target<=amount;target++){
-                int notTake=prev[target];
-                int take=0;
-                if(coins[ind]<=target){
-                    take=curr[target-coins[ind]];
+        for(int i=0;i<n;i++){
+            for(int j=arr[i];j<=amount;j++){
+                if(dp[j-arr[i]]>0){
+                    dp[j]+=dp[j-arr[i]];
                 }
-
-                curr[target]=take+notTake;
             }
-            prev=curr;
         }
-        return prev[amount];
+        return dp[amount];
     }
 };
