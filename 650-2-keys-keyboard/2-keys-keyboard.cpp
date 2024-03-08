7ecdfca9b1b0035827cbcef9fbbaf7a1abb73831
Pankaj Kumar Pandit <143678006+panditpankaj21@users.noreply.github.com>
2024-03-08 12:06:27 +0530

    Time: 36 ms (14.31%) | Memory: 163 MB (5.63%) - LeetSync

--- 650-2-keys-keyboard/2-keys-keyboard.cpp
+++ 650-2-keys-keyboard/2-keys-keyboard.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
-    int f(int count, int n, int prevCopied){
+    int f(int count, int n, int prevCopied, vector<vector<int>>& dp){
         if(count>n){
             return 1e5;
         }
         if(count==n){
             return 0;
         }
+        if(dp[count][prevCopied]!=-1) return dp[count][prevCopied];
 
-        int paste = 1 + f(count+prevCopied, n, prevCopied);
-        int copyAndpaste = 2 + f(count*2, n, count);
+        int paste = 1 + f(count+prevCopied, n, prevCopied, dp);
+        int copyAndpaste = 2 + f(count*2, n, count, dp);
 
-        return min(paste, copyAndpaste);
+        return dp[count][prevCopied]=min(paste, copyAndpaste);
 
     }
     int minSteps(int n) {
         if(n==1) return 0;
-        return 1 + f(1, n, 1);
+        vector<vector<int>> dp(n+3, vector<int>(n+5, -1));
+        return 1 + f(1, n, 1, dp);
     }
 };
