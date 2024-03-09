20c391cb2638d382335145e9785f759ad99ef4e8
Pankaj Kumar Pandit <143678006+panditpankaj21@users.noreply.github.com>
2024-03-09 14:49:37 +0530

    Time: 2 ms (60.79%) | Memory: 7 MB (66.35%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/unique-binary-search-trees.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
+    int dp[20];
     int numberOfBST(int n){
         if(n==0 || n==1){
             return 1;
         }
+        if(dp[n]!=-1) return dp[n];
         int res=0;
         for(int i=0;i<n;i++){
             res+=numberOfBST(i)*numberOfBST(n-i-1);
         }
-        return res;
+        return dp[n] = res;
     }
     int numTrees(int n) {
+        memset(dp, -1, sizeof(dp));
         return numberOfBST(n);
     }
 };
