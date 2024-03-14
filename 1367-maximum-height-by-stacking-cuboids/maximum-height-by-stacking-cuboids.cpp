71ea73db582246959f1799e5a9b532d2a4ffb667
Pankaj Kumar Pandit <143678006+panditpankaj21@users.noreply.github.com>
2024-03-14 11:16:26 +0530

    Time: 19 ms (32.81%) | Memory: 16.4 MB (27.65%) - LeetSync

--- 1367-maximum-height-by-stacking-cuboids/maximum-height-by-stacking-cuboids.cpp
+++ 1367-maximum-height-by-stacking-cuboids/maximum-height-by-stacking-cuboids.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
     // [height, length, width]
-    int maxCubHeight(int ind, int prevIndex, vector<vector<int>>& cub){
+    int maxCubHeight(int ind, int prevIndex, vector<vector<int>>& cub, vector<vector<int>>& dp){
         if(ind>=cub.size()){
             return 0;
         }
 
+        if(dp[ind][prevIndex+1]!=-1) return dp[ind][prevIndex+1];
+
         int pick = 0 , notPick = 0;
         if(prevIndex == -1 || (cub[prevIndex][0]>=cub[ind][0] && cub[prevIndex][1]>=cub[ind][1] && cub[prevIndex][2]>=cub[ind][2])){
-            pick = cub[ind][0] + maxCubHeight(ind+1, ind, cub);
+            pick = cub[ind][0] + maxCubHeight(ind+1, ind, cub, dp);
         }
-        notPick = maxCubHeight(ind+1, prevIndex, cub);
+        notPick = maxCubHeight(ind+1, prevIndex, cub, dp);
 
-        return max(pick , notPick);
+        return dp[ind][prevIndex+1] = max(pick , notPick);
     }
     int maxHeight(vector<vector<int>>& cub) {
         int n = cub.size();
@@ -27,7 +29,8 @@ public:
             }
             cout<<endl;
         }
-        return maxCubHeight(0, -1, cub);
+        vector<vector<int>> dp(n+1, vector<int>(n+2, -1));
+        return maxCubHeight(0, -1, cub, dp);
 
         
     }
